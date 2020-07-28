ea1e1376f411f879f9188e68391a88fc7c3a5973
shefyg <shefyg@gmail.com>
2020-07-28 20:08:03 +0300

    Created Ghost Element it says hi

---
+++ tryGit/Assets/Ghost.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hi 3");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
