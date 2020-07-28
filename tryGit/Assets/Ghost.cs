fe14860164326e432751fa2d84fe080d758b8bfc
JackWildBlankMask <66432466+JackWildBlankMask@users.noreply.github.com>
2020-07-28 20:53:26 +0300

    hello world

--- tryGit/Assets/Ghost.cs
+++ tryGit/Assets/Ghost.cs
@@ -7,7 +7,7 @@ public class Ghost : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hi 10");
+        Debug.Log("hii Shahar :D");
         Debug.LogError("Blank Mask Is The Best");
     }
 
